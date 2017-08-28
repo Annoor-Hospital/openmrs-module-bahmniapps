f9ee1c1d93042c09fbd37c938ecfc16b78ae9f81
Matthew <mdg583@gmail.com>
2017-08-28 09:40:19 +0300

    don't check orderSet drugs by default, move orderSet box to top of medications tab, add issue comment

--- ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
+++ ui/app/clinical/dashboard/controllers/patientDashboardTreatmentController.js
@@ -3,6 +3,7 @@
 angular.module('bahmni.clinical')
     .controller('PatientDashboardTreatmentController', ['$scope', 'ngDialog',
         function ($scope, ngDialog) {
+            // the following line is a mistake. It assumes this treament controller is being used on the dashboard. Could be used anywhere. This effectively ignores configs for treatments defined in other places than the dashboard.
             var treatmentConfigParams = $scope.dashboard.getSectionByType("treatment") || {};
             var patientUuidparams = {"patientUuid": $scope.patient.uuid};
 
