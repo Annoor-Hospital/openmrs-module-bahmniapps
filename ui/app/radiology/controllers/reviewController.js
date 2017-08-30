806a44a8679ac2bdee068f37f663d14ce5fecf33
Matthew <mdg583@gmail.com>
2017-08-30 15:53:53 +0300

    add permission for edit note, and other stuff

--- ui/app/radiology/controllers/reviewController.js
+++ ui/app/radiology/controllers/reviewController.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('radiology')
-    .controller('ReviewController', ['$scope', '$stateParams', 'spinner', '$rootScope', '$http', '$q', '$timeout', 'sessionService', '$translate', 'messagingService',
-        function ($scope, $stateParams, spinner, $rootScope, $http, $q, $timeout, sessionService, $translate, messagingService) {
+    .controller('ReviewController', ['$scope', '$stateParams', 'spinner', '$rootScope', '$http', '$q', '$timeout', 'sessionService', 'appService', '$translate', 'messagingService',
+        function ($scope, $stateParams, spinner, $rootScope, $http, $q, $timeout, sessionService, appService, $translate, messagingService) {
             $scope.targetDate = new Date();
             var init = function () {
                 var deferrables = $q.defer();
                 var promises = [];
                 // promises.push(getVisitTypes());
                 $q.all(promises).then(function () {
+                    $scope.extensions = appService.getAppDescriptor().getExtensions("bahmni.radiology") || [];
+                    if($scope.extensions.find(function(ext) {
+                        return ext.id="bahmni.radiology.editNotePrivilege";
+                    })) {
+                        $scope.canEditNote = true;
+                    }
                     deferrables.resolve();
                 });
                 return deferrables.promise;
