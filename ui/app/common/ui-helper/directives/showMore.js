4c8cb359238e6cb39b377f5067a7eadd8cbb5181
Matthew <14348557+mdg583@users.noreply.github.com>
2025-02-11 11:50:45 +0300

    Trial of show-more directive

---
+++ ui/app/common/ui-helper/directives/showMore.js
@@ -0,0 +1,53 @@
+angular.module('bahmni.common.uiHelper')
+  .directive('showMore', ['$timeout', function ($timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        console.log(element);
+        var showCount = parseInt(attrs.show, 10) || 6; // Default to 6 if not specified
+        var moreCount = parseInt(attrs.more, 10) || null; // Null means show all
+        
+        function updateVisibility() {
+          console.log(element);
+          let children = element.children();
+          console.log(children.length);
+          console.log(showCount);
+          angular.forEach(children, function(child, index) {
+            angular.element(child).css('display', index < showCount ? '' : 'none');
+          });
+
+          if (showCount < children.length) {
+            showMoreBtn.css('display', 'block');
+          } else {
+            showMoreBtn.css('display', 'none');
+          }
+        }
+
+        // Add "Show More" button
+        var showMoreBtn = angular.element('<div>Show More...</div>');
+        showMoreBtn.css({
+          display: 'block',
+          cursor: 'pointer',
+          margin: '5px',
+          fontWeight: 'bold'
+        });
+
+        showMoreBtn.on('click', function() {
+          let children = element.children();
+          console.log(children.length);
+          scope.$apply(function() {
+            if (moreCount) {
+              showCount = Math.min(showCount + moreCount, children.length);
+            } else {
+              showCount = children.length; // Show all if "more" is not provided
+            }
+            updateVisibility();
+          });
+        });
+
+        element.after(showMoreBtn);
+        //updateVisibility();
+        $timeout(updateVisibility, 0);
+      }
+    };
+  }]);
